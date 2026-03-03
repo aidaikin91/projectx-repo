b8cc7cddd42c80641b3867a505481e0abf9abd53
aidaikin91 <abeishibekova@gmail.com>
2026-03-03 00:33:01 -0600

    chore(monitoring): add baseline eks-monitoring manifests

--- roots/devops-project-main/backend.tf
+++ roots/devops-project-main/backend.tf
@@ -2,10 +2,10 @@ terraform {
   required_version = ">= 1.6.6"
 
   backend "s3" {
-    bucket         = "665832051028-terraform-state-dev"
-    key            = "dev/terraform.tfstate"
+    bucket         = "665832051028-projectx-tfstate"
+    key            = "projectx/devops-project-main/dev/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "665832051028-terraform-lock-dev"
+    dynamodb_table = "060623762260-terraform-lock-dev"
   }
 }
